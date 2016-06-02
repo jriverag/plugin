f7a23dbecf72e2b7194700b074caea74752d54e1
jriverag <jrivera@uab.edu>
2016-06-02 14:45:00 -0500

    Added Script File
    Script file with app functions added

---
+++ js/plugscript.js
@@ -0,0 +1,61 @@
+function PickContact()
+{
+    navigator.contacts.pickContact(function(contact)
+        {
+            var contactinfo = ""
+            if (contact.displayName !== null)
+            {
+                contactinfo += contact.displayName + "<br>";
+            }
+            else
+            {
+                contactinfo += contact.name.givenName + " " + contact.name.familyName + "<br>";
+            }
+            var count = 0;
+            if (contact.phoneNumbers !== null)
+            {
+                for (count=0; count < contact.phoneNumbers.length; count++)
+                {
+                    contactinfo += contact.phoneNumbers[count].type + ": " + contact.phoneNumbers[count].value + "<br>";
+                }
+            }
+            if (contact.emails !== null)
+            {
+                for(count=0; count < contact.emails.length; count++)
+                {
+                    contactinfo += contact.emails[count].type + ": " + contact.emails[count].value + "<br>";
+                }
+            }
+            document.getElementById("contactname").innerHTML = contactinfo;
+            alert("The following contact has been selected: " + JSON.stringify(contact));
+        }, function(err)
+        {
+            alert("Error: " + err);
+        }
+        );
+}
+
+function onPhotoDataSuccess(imagedata)
+{
+    var smallImage = document.getElementById("smallImage");
+    smallImage.style.display = 'block';
+    smallImage.src = "data:image/jpeg;base64," + imagedata;
+}
+
+function onPhotoURISuccess(imageURI)
+{
+    var largeImage = document.getElementById("largeImage");
+    largeImage.style.display = 'block';
+    largeImage.src = imageURI;
+}
+
+function CapturePhoto()
+{
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true, destinationtype: destinationtype.DATA_URL });
+    
+}
+
+function onFail(message)
+{
+    alert("Failed because: " + message);
+}
