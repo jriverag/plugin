027ba701fcce2c1e2f6b8082d64390762764063b
jriverag <jrivera@uab.edu>
2016-06-10 12:16:12 -0500

    App Updates
    Modified HTML & scripts for more modularity

--- js/plugscript.js
+++ js/plugscript.js
@@ -1,38 +1,73 @@
 function PickContact()
 {
-    navigator.contacts.pickContact(function(contact)
+    navigator.contacts.pickcontacts(FoundContact(contact),NoContact(err));
+    //navigator.contacts.pickContact(function(contact)
+    //    {
+    //        var contactinfo = ""
+    //        if (contact.displayName !== null)
+    //        {
+    //            contactinfo += contact.displayName + "<br>";
+    //        }
+    //        else
+    //        {
+    //            contactinfo += contact.name.givenName + " " + contact.name.familyName + "<br>";
+    //        }
+    //        var count = 0;
+    //        if (contact.phoneNumbers !== null)
+    //        {
+    //            for (count=0; count < contact.phoneNumbers.length; count++)
+    //            {
+    //                contactinfo += contact.phoneNumbers[count].type + ": " + contact.phoneNumbers[count].value + "<br>";
+    //            }
+    //        }
+    //        if (contact.emails !== null)
+    //        {
+    //            for(count=0; count < contact.emails.length; count++)
+    //            {
+    //                contactinfo += contact.emails[count].type + ": " + contact.emails[count].value + "<br>";
+    //            }
+    //        }
+    //        document.getElementById("contactname").innerHTML = contactinfo;
+    //        alert("The following contact has been selected: " + JSON.stringify(contact));
+    //    }, function(err)
+    //    {
+    //        alert("Error: " + err);
+    //    }
+    //    );
+}
+
+function FoundContact(contactitem)
+{
+    var contactinfo = "";
+    if (contact.displayName !== null)
         {
-            var contactinfo = ""
-            if (contact.displayName !== null)
+            contactinfo += contactitem.displayName + "<br>";
+        }
+        else
+        {
+            contactinfo += contactitem.name.givenName + " " + contactitem.name.familyName + "<br>";
+        }
+        var count = 0;
+        if (contactitem.phoneNumbers !== null)
+        {
+            for (count=0; count < contactitem.phoneNumbers.length; count++)
             {
-                contactinfo += contact.displayName + "<br>";
+                contactinfo += contactitem.phoneNumbers[count].type + ": " + contactitem.phoneNumbers[count].value + "<br>";
             }
-            else
+        if (contact.emails !== null)
             {
-                contactinfo += contact.name.givenName + " " + contact.name.familyName + "<br>";
-            }
-            var count = 0;
-            if (contact.phoneNumbers !== null)
-            {
-                for (count=0; count < contact.phoneNumbers.length; count++)
+                for(count=0; count < contactitem.emails.length; count++)
                 {
-                    contactinfo += contact.phoneNumbers[count].type + ": " + contact.phoneNumbers[count].value + "<br>";
-                }
-            }
-            if (contact.emails !== null)
-            {
-                for(count=0; count < contact.emails.length; count++)
-                {
-                    contactinfo += contact.emails[count].type + ": " + contact.emails[count].value + "<br>";
+                    contactinfo += contactitem.emails[count].type + ": " + contactitem.emails[count].value + "<br>";
                 }
             }
             document.getElementById("contactname").innerHTML = contactinfo;
-            alert("The following contact has been selected: " + JSON.stringify(contact));
-        }, function(err)
-        {
-            alert("Error: " + err);
-        }
-        );
+            alert("The following contact has been selected: " + JSON.stringify(contactitem));
+}
+
+function NoContact(message)
+{
+    alert("Error: " + message);
 }
 
 function onPhotoURISuccess(imageURI)
