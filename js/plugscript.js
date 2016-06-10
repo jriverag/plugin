ac4e5f820828ccbaa768d23c882691073f640f5f
jriverag <jrivera@uab.edu>
2016-06-10 14:50:00 -0500

    Cleaned up Code
    Cleanup of prickcontact code

--- js/plugscript.js
+++ js/plugscript.js
@@ -1,87 +1,38 @@
-function onSuccess(item)
-    {
-        alert("Found Contacts" + JSON.stringify(item));
-    }
-function onError(message)
-    {
-        alert("Error: " + message);
-    }
-
 function PickContact()
 {
-    navigator.contacts.pickcontacts(function(contact){onSuccess(contact);},function(err){onError(err);});
+    navigator.contacts.pickContact(function(contact)
+        {
+            var contactinfo = "";
+            if (contact.displayName !== null)
+            {
+                contactinfo += contact.displayName + "<br>";
+            }
+            else
+            {
+                contactinfo += contact.name.givenName + " " + contact.name.familyName + "<br>";
+            }
+            var count = 0;
+            if (contact.phoneNumbers !== null)
+            {
+                for (count=0; count < contact.phoneNumbers.length; count++)
+                {
+                    contactinfo += contact.phoneNumbers[count].type + ": " + contact.phoneNumbers[count].value + "<br>";
+                }
+            }
+            if (contact.emails !== null)
+            {
+                for(count=0; count < contact.emails.length; count++)
+                {
+                    contactinfo += contact.emails[count].type + ": " + contact.emails[count].value + "<br>";
+                }
+            }
+            document.getElementById("contactname").innerHTML = contactinfo;
+        }, function(err)
+        {
+            alert("Error: " + err);
+        }
+        );
 }
-    //navigator.contacts.pickContact(function(contact)
-    //    {
-    //        var contactinfo = "";
-    //        if (contact.displayName !== null)
-    //        {
-    //            contactinfo += contact.displayName + "<br>";
-    //        }
-    //        else
-    //        {
-    //            contactinfo += contact.name.givenName + " " + contact.name.familyName + "<br>";
-    //        }
-    //        var count = 0;
-    //        if (contact.phoneNumbers !== null)
-    //        {
-    //            for (count=0; count < contact.phoneNumbers.length; count++)
-    //            {
-    //                contactinfo += contact.phoneNumbers[count].type + ": " + contact.phoneNumbers[count].value + "<br>";
-    //            }
-    //        }
-    //        if (contact.emails !== null)
-    //        {
-    //            for(count=0; count < contact.emails.length; count++)
-    //            {
-    //                contactinfo += contact.emails[count].type + ": " + contact.emails[count].value + "<br>";
-    //            }
-    //        }
-    //        document.getElementById("contactname").innerHTML = contactinfo;
-    //        alert("The following contact has been selected: " + JSON.stringify(contact));
-    //    }, function(err)
-    //    {
-    //        alert("Error: " + err);
-    //    }
-    //    );
-//}
-
-//function FoundContact(contact)
-//    {
-//        alert("Found Contacts" + JSON.stringify(contact));
-//    }
-//    var contactinfo = "";
-//    if (contact.displayName !== null)
-//    {
-//        contactinfo += contact.displayName + "<br>";
-//    }
-//    else
-//    {
-//        contactinfo += contact.name.givenName + " " + contact.name.familyName + "<br>";
-//    }
-//    var count = 0;
-//    if (contact.phoneNumbers !== null)
-//    {
-//        for (count=0; count < contact.phoneNumbers.length; count++)
-//        {
-//            contactinfo += contact.phoneNumbers[count].type + ": " + contact.phoneNumbers[count].value + "<br>";
-//        }
-//    }
-//    if (contact.emails !== null)
-//    {
-//        for(count=0; count < contact.emails.length; count++)
-//        {
-//            contactinfo += contact.emails[count].type + ": " + contact.emails[count].value + "<br>";
-//        }
-//    }
-//    document.getElementById("contactname").innerHTML = contactinfo;
-//    alert("The following contact has been selected: " + JSON.stringify(contact));
-//}
-//
-//function NoContact(err)
-//{
-//    alert("Error: " + err);
-//}
 
 function onPhotoURISuccess(imageURI)
 {
