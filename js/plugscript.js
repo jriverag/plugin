8c076a474af2261c5146ad4fdc6cffb5d832fd1f
jriverag <jrivera@uab.edu>
2016-06-10 12:30:54 -0500

    Code Corrections
    Corrected Function Code

--- js/plugscript.js
+++ js/plugscript.js
@@ -39,30 +39,31 @@ function PickContact()
 function FoundContact(contactitem)
 {
     var contactinfo = "";
-    if (contact.displayName !== null)
+    if (contactitem.displayName !== null)
+    {
+        contactinfo += contactitem.displayName + "<br>";
+    }
+    else
+    {
+        contactinfo += contactitem.name.givenName + " " + contactitem.name.familyName + "<br>";
+    }
+    var count = 0;
+    if (contactitem.phoneNumbers !== null)
+    {
+        for (count=0; count < contactitem.phoneNumbers.length; count++)
         {
-            contactinfo += contactitem.displayName + "<br>";
+            contactinfo += contactitem.phoneNumbers[count].type + ": " + contactitem.phoneNumbers[count].value + "<br>";
         }
-        else
+    }
+    if (contactitem.emails !== null)
+    {
+        for(count=0; count < contactitem.emails.length; count++)
         {
-            contactinfo += contactitem.name.givenName + " " + contactitem.name.familyName + "<br>";
+            contactinfo += contactitem.emails[count].type + ": " + contactitem.emails[count].value + "<br>";
         }
-        var count = 0;
-        if (contactitem.phoneNumbers !== null)
-        {
-            for (count=0; count < contactitem.phoneNumbers.length; count++)
-            {
-                contactinfo += contactitem.phoneNumbers[count].type + ": " + contactitem.phoneNumbers[count].value + "<br>";
-            }
-        if (contact.emails !== null)
-            {
-                for(count=0; count < contactitem.emails.length; count++)
-                {
-                    contactinfo += contactitem.emails[count].type + ": " + contactitem.emails[count].value + "<br>";
-                }
-            }
-            document.getElementById("contactname").innerHTML = contactinfo;
-            alert("The following contact has been selected: " + JSON.stringify(contactitem));
+    }
+    document.getElementById("contactname").innerHTML = contactinfo;
+    alert("The following contact has been selected: " + JSON.stringify(contactitem));
 }
 
 function NoContact(message)
